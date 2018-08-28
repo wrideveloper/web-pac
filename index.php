fdd6612a3f6c78ed63f448fa0d15ea87493c60b2
mnindrazaka <mnindrazaka@gmail.com>
2018-08-28 16:13:31 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,387 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+  <meta charset="utf-8">
+  <title>PAC</title>
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="">
+  <meta property="og:site_name" content="">
+  <meta property="og:description" content="">
+
+  <!-- Styles -->
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/animate.css">
+  <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900|Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <script src="js/modernizr-2.7.1.js"></script>
+
+</head>
+
+<body>
+
+
+  <div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="index.php"><img src="img/logo.svg" alt="Logo" class="logo"></a>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#timeline" class="scroll">Timeline</a></li>
+          <li><a href="#hadiah" class="scroll">Hadiah</a></li>
+          <li><a href="#ketentuan" class="scroll">Ketentuan</a></li>
+          <li><a href="https://bit.ly/pac_2017">Register</a></li>
+          <li><a href="https://goo.gl/cV5yyj">Template Proposal</a></li>
+        </ul>
+      </div><!--/.navbar-collapse -->
+    </div>
+  </div>
+
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-6">
+          <a href="index.php"><img src="img/logo.svg" alt="Logo" class="logo"></a>
+        </div>
+        <div class="col-xs-6 signin text-right navbar-nav">
+          <a href="#timeline" class="scroll">Timeline</a>&nbsp; &nbsp; &nbsp;
+          <a href="#hadiah" class="scroll">Hadiah</a>&nbsp; &nbsp; &nbsp;
+          <a href="#ketentuan" class="scroll">Ketentuan</a>&nbsp; &nbsp; &nbsp;
+          <a href="https://bit.ly/pac_2017">Register</a>&nbsp; &nbsp; &nbsp;
+          <a href="https://goo.gl/cV5yyj">Template Proposal</a>
+        </div>
+      </div>
+
+      <div class="row header-info">
+        <div class="col-sm-10 col-sm-offset-1 text-center">
+          <h1 class="wow fadeIn">Polinema Mobile Apps And Game Competition</h1>
+          <br />
+          <p class="lead wow fadeIn" data-wow-delay="0.5s">PAC diselenggarakan oleh Jurusan Teknologi Informasi Politeknik Negeri Malang,
+            sebagai wadah berkompetisi bagi developer muda yang ingin berkreasi pada bidang pembuatan Aplikasi Mobile.</p>
+            <br />
+
+            <div class="row">
+              <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+                <div class="row">
+                  <div class="col-xs-6 text-right wow fadeInUp" data-wow-delay="1s">
+                    <a href="#be-the-first" class="btn btn-secondary btn-lg scroll">Tentang PAC</a>
+                  </div>
+                  <div class="col-xs-6 text-left wow fadeInUp" data-wow-delay="1.4s">
+                    <a href="https://bit.ly/pac_2017" class="btn btn-primary btn-lg scroll">REGISTER</a>
+                  </div>
+                </div><!--End Button Row-->
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <div class="mouse-icon hidden-xs">
+      <div class="scroll"></div>
+    </div>
+
+    <section id="be-the-first" class="">
+      <div class="cover_head pad-sm">
+
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12 margin-30 wow fadeIn" data-wow-delay="0.6s">
+              <div class="col-lg-6 col-sm-12">
+                <h2>Tentang PAC</h2>
+                <p class="lead">PAC adalah singkatan dari <b>Polinema Apps Competition</b>,
+                  pertama kali diselenggarakan oleh Jurusan Teknologi Informasi Politeknik Negeri Malang,
+                  sebagai wadah berkompetisi bagi developer muda yang ingin berkreasi pada bidang pembuatan Aplikasi Mobile.
+                </p>
+                <p class="lead">
+                  Kompetisi ini terbuka bagi seluruh mahasisa di indonesia, dari segala jurusan,
+                  terutama yang memiliki kemampuan di bidang ilmu teknolog dan informasi,
+                  untuk mengembangkan aplikasi mobile dan game yang <b>kreatif, inovatif, dan beruna bagi masyarakat <small class="blue">#AyoNgodingMobile</small></b>
+                </p>
+              </div>
+              <div class="col-lg-6 col-sm-12">
+                <div class="row">
+
+                  <h2>Persyaratan Umum</h2>
+                  <ul>
+                    <li class="lead">Perguruan Tinggi peserta adalah perguruan tinggi yang terdaftar pada laman PD DIKTI (<a href="http://forlap.dikti.go.id">http://forlap.dikti.go.id</a>)</li>
+                    <li class="lead">Peserta adalah mahasiswa aktif Program Diploma atau Strata 1 dengan umur maksimum 24 tahun pada bulan November 2017 ketika PAC berlangsung.</li>
+                    <li class="lead">Aplikasi yang dilombakan belum pernah dipublikasikan di media apapun.</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="iphone wow fadeInUp" data-wow-delay="1s">
+            <img src="img/iphone.png">
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <section id="timeline" class="pad-xl">
+      <div class="container">
+        <div class="timeline wow fadeIn" data-wow-delay="0.8s">
+          <h2>Timeline</h2>
+        </div>
+        <div class="row">
+          <div class="col-sm-4 wow fadeIn" data-wow-delay="0.4s">
+            <hr class="line green">
+            <h3>Pendaftaran</h3>
+            <p>Pendaftaran dibuka mulai 1 September 2017 - 30 September 2017.</p>
+          </div>
+          <div class="col-sm-4 wow fadeIn" data-wow-delay="0.8s">
+            <hr class="line purple">
+            <h3>Babak Penyisihan</h3>
+            <p>Submisi akhir proposal tahap 1 paling lambat 30 September 2017 dengan cara upload file proposal ke E-Mail : <b class="blue">pac@polinema.ac.id</b> </p>
+          </div>
+          <div class="col-sm-4 wow fadeIn" data-wow-delay="1.2s">
+            <hr  class="line blue">
+            <h3>Pengumuman Babak Penyisihan</h3>
+            <p>Pengumuman babak penyisihan tahap 1 akan di umumkan tanggal 2 Oktober 2017.</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <hr>
+          <div class="col-sm-4 wow fadeIn" data-wow-delay="1.6s">
+            <hr  class="line blue">
+            <h3>Pengumpulan Video Demo Aplikasi</h3>
+            <p>Pengumpulan terakhir demo aplikasi berupa video demo yang diupload ke youtube, kemudian manual instalasi dan aplikasi yang telah dibuat dikirim ke E-mail PAC beserta link youtube dari video demo aplikasi.
+              Pengumpulan paling lambat tanggal 23 Oktober 2017.</p>
+            </div>
+            <div class="col-sm-4 wow fadeIn" data-wow-delay="1.9s">
+              <hr  class="line yellow">
+              <h3>Pengumuman Finalis</h3>
+              <p>Pengumuman peserta yang lolos ke babak final akan di umumkan tanggal 25 Oktober 2017.</p>
+            </div>
+            <div class="col-sm-4 wow fadeIn" data-wow-delay="2.2s">
+              <hr  class="line green">
+              <h3>Final</h3>
+              <p>Final lomba Polinema Mobile Apps And Game Competition tanggal 28 Oktober 2017.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <!--hadiah-->
+      <section id="hadiah" class="pad-sm">
+        <div class="container">
+          <div class="row margin-40">
+            <div class="col-sm-8 col-sm-offset-2 text-center wow fadeInUp" data-wow-delay="0.2s">
+              <h2 class="white">Hadiah</h2>
+              <p class="white">Hadiah yang akan di berikan kepada peserta yang berhasil memenangkan lomba PAC.</p>
+            </div>
+          </div>
+
+          <div class="row margin-50">
+
+            <div class="col-sm-4 pricing-container wow fadeInUp" data-wow-delay="1s">
+              <br />
+              <ul class="list-unstyled pricing-table text-center">
+                <li class="headline"><h5 class="white">Juara 2</h5></li>
+                <li class="price"><div class="amount"><small>Rp.</small> 2.250.000</div></li>
+                <li class="info">Setiap pemenang juara 2 lomba Polinema Mobile Apps And Game Competition akan mendapatkan</li>
+                <li class="features first">Piagam</li>
+                <li class="features">Sertifikat</li>
+              </ul>
+            </div>
+
+            <div class="col-sm-4 pricing-container wow fadeInUp" data-wow-delay="0.4s">
+              <ul class="list-unstyled pricing-table active text-center">
+                <li class="headline"><h5 class="white">Juara 1</h5></li>
+                <li class="price"><div class="amount"><small>Rp.</small> 3.000.000</div></li>
+                <li class="info">Setiap pemenang juara 1 lomba Polinema Mobile Apps And Game Competition akan mendapatkan</li>
+                <li class="features first">Piagam</li>
+                <li class="features">Sertifikat</li>
+                <li class="features last btn btn-secondary btn-wide"><a href="hadiah.php">Lihat semua hadiah</a></li>
+              </ul>
+            </div>
+
+            <div class="col-sm-4 pricing-container wow fadeInUp" data-wow-delay="1.3s">
+              <br />
+              <ul class="list-unstyled pricing-table text-center">
+                <li class="headline"><h5 class="white">Juara 3</h5></li>
+                <li class="price"><div class="amount"><small>Rp.</small> 850.000</div></li>
+                <li class="info">Setiap pemenang juara 3 lomba Polinema Mobile Apps And Game Competition akan mendapatkan</li>
+                <li class="features first">Piagam</li>
+                <li class="features">Sertifikat</li>
+              </ul>
+            </div>
+
+          </div>
+
+          <div class="row margin-40">
+            <div class="col-sm-8 col-sm-offset-2 text-center wow fadeInUp" data-wow-delay="1.6s">
+              <h4 class="white">Juara Harapan</h4>
+              <p class="white">Harapan 1 : 650.000 </p>
+              <hr>
+              <p class="white">Harapan 2 : 650.000 </p>
+              <hr>
+              <p class="white">Harapan 3 : 650.000 </p>
+              <hr>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+
+      <section id="ketentuan" class="pad-lg light-gray-bg">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 col-sm-offset-2 wow fadeIn" data-wow-delay="0.6s">
+              <h2 class="text-center">Ketentuan dan Prosedur pendaftaran PAC</h2>
+              <br />
+              <p class="bold">Ketentuan peserta kompetisi :
+                <br/><br/>
+                <ul>
+                  <li class="lead">Peserta lomba merupakan tim yang beranggotakan <b>maksimal 3 orang.</b></li>
+                  <li class="lead">Melengkapi data ketua dan anggota tim serta melampirkan scan KTM.</li>
+                </ul>
+              </p>
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="ketentuan" class="pad-lg light-gray-bg">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 col-sm-offset-2 wow fadeIn" data-wow-delay="0.6s">
+              <h2 class="text-center">Finasil Lomba PAC</h2>
+              <br />
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Nama Tim</th>
+                    <th>Asal Perguruan Tinggi</th>
+                    <th>Nama Aplikasi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>BCC SPESIALKU</td>
+                    <td>Universitas Brawijaya</td>
+                    <td>Come</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Kamalogis</td>
+                    <td>Universitas Gadjah Mada</td>
+                    <td>Kreatoria</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Plug In Team</td>
+                    <td>Universitas Trunojoyo Madura</td>
+                    <td>MotorAssis</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">4</th>
+                    <td>SEPAY Team</td>
+                    <td>Politeknik Elektronika Negeri Surabaya</td>
+                    <td>Food Tycon</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">5</th>
+                    <td>TEAM LAFIC</td>
+                    <td>Politeknik Negeri Malang</td>
+                    <td>Lafic</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">6</th>
+                    <td>GXB</td>
+                    <td>Binus University</td>
+                    <td>Dr. Toro</td>
+                  </tr>
+                </tbody>
+              </table>
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section id="media" class="pad-sm">
+        <div class="container">
+
+          <div class="row margin-30 news-container">
+            <h2 class="text-center wow fadeInLeft" data-wow-delay="0.4s">Media Partner</h2>
+            <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 wow fadeInLeft" data-wow-delay="0.8s">
+              <div class="col-md-6">
+                <a href="#">
+                  <img class="news-img right" src="img/hmti.jpg" alt="HMTI">
+                </a>
+              </div>
+              <div class="col-md-6">
+                <a href="wri.polinema.ac.id">
+                  <img class="news-img center" src="img/wri.jpg" alt="WRI">
+                </a>
+              </div>
+              <!-- <a href="http://wri.polinema.ac.id/">
+              <img class="news-img center" src="img/wri.jpg" alt="WRI">
+            </a> -->
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+
+    <footer>
+      <div class="container">
+
+        <div class="row">
+          <div class="col-sm-8 margin-20">
+            <ul class="list-inline social">
+              <li>Connect with us on</li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+              <li><a href="mailto:pac@polinema.ac.id"><i class="fa fa-envelope"></i></a></li>
+            </ul>
+          </div>
+
+          <div class="col-sm-4 text-right">
+            <p><small>Copyright &copy; 2017. All rights reserved. <br>
+            </div>
+          </div>
+
+        </div>
+      </footer>
+
+
+      <!-- Javascript
+      ================================================== -->
+      <!-- Placed at the end of the document so the pages load faster -->
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+      <script>window.jQuery || document.write('<script src="js/jquery-1.11.0.min.js"><\/script>')</script>
+      <script src="js/wow.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/main.js"></script>
+
+
+    </body>
+    </html>
