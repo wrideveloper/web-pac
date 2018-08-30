a81733310ade6684867c8c89d9bb0105087f6c1d
mnindrazaka <mnindrazaka@gmail.com>
2018-08-31 00:13:46 +0700

    redesign timeline

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
 
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/timeline.css">
 
   <script src="js/modernizr-2.7.1.js"></script>
 
@@ -76,10 +77,9 @@
             sebagai wadah berkompetisi bagi developer muda yang ingin berkreasi pada bidang pembuatan Aplikasi Mobile.</p>
           <br />
 
-          <div class="btn-group" role="group" aria-label="">
-            <a href="#be-the-first" class="btn btn-primary btn-lg">Tentang PAC</a>
-            <a href="https://bit.ly/pac_2017" class="btn btn-success btn-lg">Daftar</a>
-          </div>      
+          <a href="https://bit.ly/pac_2017" class="btn btn-lg btn-success" style="font-weight: bold">Daftar</a>
+          &nbsp;
+          <a href="#be-the-first" class="btn btn-lg btn-outline-primary">Tentang PAC</a>
                     
         </div>
         <div class="col-md-6">
@@ -88,13 +88,12 @@
       </div>
   </header>
 
-    <section id="be-the-first" class="">
+    <section id="be-the-first">
       <div class="cover_head pad-sm">
-
         <div class="container">
           <div class="row">
             <div class="col-sm-12 margin-30 wow fadeIn" data-wow-delay="0.6s">
-              <div class="col-lg-6 col-sm-12">
+              <div class="col-lg-6 col-sm-12 padding border">
                 <h2>Tentang PAC</h2>
                 <p class="lead">PAC adalah singkatan dari <b>Polinema Apps Competition</b>,
                   pertama kali diselenggarakan oleh Jurusan Teknologi Informasi Politeknik Negeri Malang,
@@ -106,72 +105,61 @@
                   untuk mengembangkan aplikasi mobile dan game yang <b>kreatif, inovatif, dan berguna bagi masyarakat <small class="blue">#AyoNgodingMobile</small></b>
                 </p>
               </div>
-              <div class="col-lg-6 col-sm-12">
-                <div class="row">
-
+              <div class="col-lg-6 col-sm-12 padding">
                   <h2>Persyaratan Umum</h2>
                   <ul>
-                    <li class="lead">Perguruan Tinggi peserta adalah perguruan tinggi yang terdaftar pada laman PD DIKTI (<a href="http://forlap.dikti.go.id">http://forlap.dikti.go.id</a>)</li>
-                    <li class="lead">Peserta adalah mahasiswa aktif Program Diploma atau Strata 1 dengan umur maksimum 24 tahun pada bulan November 2017 ketika PAC berlangsung.</li>
-                    <li class="lead">Aplikasi yang dilombakan belum pernah dipublikasikan di media apapun.</li>
+                      <li>Perguruan Tinggi peserta adalah perguruan tinggi yang terdaftar pada laman PD DIKTI (<a href="http://forlap.dikti.go.id">http://forlap.dikti.go.id</a>)</li>
+                      <br/>
+                      <li>Peserta adalah mahasiswa aktif Program Diploma atau Strata 1 dengan umur maksimum 24 tahun pada bulan November 2017 ketika PAC berlangsung.</li>
+                      <br/>
+                      <li>Aplikasi yang dilombakan belum pernah dipublikasikan di media apapun.</li>
                   </ul>
-                </div>
               </div>
             </div>
-          </div>
-
-          <div class="iphone wow fadeInUp" data-wow-delay="1s">
-            <img src="img/iphone.png">
           </div>
         </div>
 
       </div>
     </section>
 
-    <section id="timeline" class="pad-xl">
+    <section id="timeline" style="padding: 50px">
       <div class="container">
-        <div class="timeline wow fadeIn" data-wow-delay="0.8s">
+       <div class="row">
+        <div class="col-md-12">
+           <div class="timeline wow fadeIn" data-wow-delay="0.8s">
           <h2>Timeline</h2>
-        </div>
-        <div class="row">
-          <div class="col-sm-4 wow fadeIn" data-wow-delay="0.4s">
-            <hr class="line green">
-            <h3>Pendaftaran</h3>
-            <p>Pendaftaran dibuka mulai <b>1 September 2018 - 30 September 2018</b></p>
-          </div>
-          <div class="col-sm-4 wow fadeIn" data-wow-delay="0.8s">
-            <hr class="line purple">
-            <h3>Babak Penyisihan</h3>
-            <p>Submisi akhir proposal tahap 1 paling lambat <b>30 September 2018</b> dengan cara upload file proposal ke E-Mail : <b class="blue">pac@polinema.ac.id</b> </p>
-          </div>
-          <div class="col-sm-4 wow fadeIn" data-wow-delay="1.2s">
-            <hr  class="line blue">
-            <h3>Pengumuman Babak Penyisihan</h3>
-            <p>Pengumuman babak penyisihan tahap 1 akan di umumkan tanggal <b>2 Oktober 2018</b></p>
-          </div>
-        </div>
-
-        <div class="row">
-          <hr>
-          <div class="col-sm-4 wow fadeIn" data-wow-delay="1.6s">
-            <hr  class="line blue">
-            <h3>Pengumpulan Video Demo Aplikasi</h3>
-            <p>Pengumpulan terakhir demo aplikasi berupa video demo yang diupload ke youtube, kemudian manual instalasi dan aplikasi yang telah dibuat dikirim ke E-mail PAC beserta link youtube dari video demo aplikasi.
+          <ul class="timeline">
+				<li>
+					<a target="_blank" href="https://www.totoprayogo.com/#">Pendaftaran</a>
+					<p>Pendaftaran dibuka mulai <b>1 September 2018 - 30 September 2018</b></p>
+				</li>
+				<li>
+					<a href="#">Babak Penyisihan</a>
+					<p>Submisi akhir proposal tahap 1 paling lambat <b>30 September 2018</b> dengan cara upload file proposal ke E-Mail : <b class="blue">pac@polinema.ac.id</b></p>
+				</li>
+				<li>
+					<a href="#">Pengumuman Babak Penyisihan</a>
+					<p>Pengumuman babak penyisihan tahap 1 akan di umumkan tanggal <b>2 Oktober 2018</b></p>
+				</li>
+        <li>
+					<a href="#">Pengumpulan Video Demo Aplikasi</a>
+					<p>Pengumpulan terakhir demo aplikasi berupa video demo yang diupload ke youtube, kemudian manual instalasi dan aplikasi yang telah dibuat dikirim ke E-mail PAC beserta link youtube dari video demo aplikasi.
               Pengumpulan paling lambat tanggal <b>22 Oktober 2018</b></p>
-            </div>
-            <div class="col-sm-4 wow fadeIn" data-wow-delay="1.9s">
-              <hr  class="line yellow">
-              <h3>Pengumuman Finalis</h3>
-              <p>Pengumuman peserta yang lolos ke babak final akan di umumkan tanggal <b>24 Oktober 2018</b></p>
-            </div>
-            <div class="col-sm-4 wow fadeIn" data-wow-delay="2.2s">
-              <hr  class="line green">
-              <h3>Final</h3>
-              <p>Final lomba Polinema Mobile Apps And Game Competition tanggal <b>29 Oktober 2018</b></p>
-            </div>
-          </div>
+				</li>
+        <li>
+					<a href="#">Pengumuman Finalis</a>
+					<p>Pengumuman peserta yang lolos ke babak final akan di umumkan tanggal <b>24 Oktober 2018</b></p>
+				</li>
+        <li>
+					<a href="#">Final</a>
+					<p>Final lomba Polinema Mobile Apps And Game Competition tanggal <b>29 Oktober 2018</b></p>
+				</li>
+			</ul>
         </div>
-      </section>
+         </div>
+       </div>
+      </div>
+    </section>
 
 
       <!--hadiah-->
@@ -204,7 +192,6 @@
                 <li class="info">Setiap pemenang juara 1 lomba Polinema Mobile Apps And Game Competition akan mendapatkan</li>
                 <li class="features first">Piagam</li>
                 <li class="features">Sertifikat</li>
-                <li class="features last btn btn-secondary btn-wide"><a href="hadiah.php">Lihat semua hadiah</a></li>
               </ul>
             </div>
 
@@ -262,66 +249,6 @@
         </div>
       </section>
 
-      <section id="ketentuan" class="pad-lg light-gray-bg">
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-8 col-sm-offset-2 wow fadeIn" data-wow-delay="0.6s">
-              <h2 class="text-center">Finasil Lomba PAC</h2>
-              <br />
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Nama Tim</th>
-                    <th>Asal Perguruan Tinggi</th>
-                    <th>Nama Aplikasi</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>BCC SPESIALKU</td>
-                    <td>Universitas Brawijaya</td>
-                    <td>Come</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Kamalogis</td>
-                    <td>Universitas Gadjah Mada</td>
-                    <td>Kreatoria</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Plug In Team</td>
-                    <td>Universitas Trunojoyo Madura</td>
-                    <td>MotorAssis</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">4</th>
-                    <td>SEPAY Team</td>
-                    <td>Politeknik Elektronika Negeri Surabaya</td>
-                    <td>Food Tycon</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">5</th>
-                    <td>TEAM LAFIC</td>
-                    <td>Politeknik Negeri Malang</td>
-                    <td>Lafic</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">6</th>
-                    <td>GXB</td>
-                    <td>Binus University</td>
-                    <td>Dr. Toro</td>
-                  </tr>
-                </tbody>
-              </table>
-
-            </div>
-          </div>
-        </div>
-      </section>
-
 
       <section id="media" class="pad-sm">
         <div class="container">
@@ -353,18 +280,8 @@
       <div class="container">
 
         <div class="row">
-          <div class="col-sm-8 margin-20">
-            <ul class="list-inline social">
-              <li>Connect with us on</li>
-              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-              <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-              <li><a href="mailto:pac@polinema.ac.id"><i class="fa fa-envelope"></i></a></li>
-            </ul>
-          </div>
-
-          <div class="col-sm-4 text-right">
-            <p><small>Copyright &copy; 2017. All rights reserved. <br>
+          <div class="col-sm-12 text-center">
+            <p><small>Copyright &copy; 2018. All rights reserved. <br>
             </div>
           </div>
 
